92a45112ca437027f30e4d90d53f132d696a55c8
Ivan Kipyegon <ivan@bardo.com>
2017-09-19 18:22:12 +0300

    feat(): Refactor getUsersById method to use array input

--- database/mdb.js
+++ database/mdb.js
@@ -1,10 +1,14 @@
-module.exports = mPoool => {
+const { orderedFor } = require('../lib/util');
 
+module.exports = mPool => {
   return {
-    getCounts(user, countsField) {
-      return mPoool.collection('users')
-        .findOne({userId: user.id})
-        .then(userCounts => userCounts[countsField])
+    getUsersById(userIds) {
+      return mPool.collection('users')
+        .find({ userId: { $in: userIds }})
+        .toArray()
+        .then(rows => {
+          return orderedFor(rows, userIds, 'userId', true)
+        })
     }
-  }
+  };
 };
