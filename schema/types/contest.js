1510c44ed9738c184f5f5e365f0b238d346f24a1
Ivan Kipyegon <ivan@bardo.com>
2017-09-19 18:19:51 +0300

    chore(): Add names field to contest type schema

--- schema/types/contest.js
+++ schema/types/contest.js
@@ -1,22 +1,32 @@
 const {
   GraphQLObjectType,
-  GraphQLNonNull,
+  GraphQLID,
   GraphQLString,
-  GraphQLID
+  GraphQLNonNull,
+  GraphQLList
 } = require('graphql');
 
-const ContestStatusType = require('./contest-status');
+
 
 module.exports = new GraphQLObjectType({
   name: 'ContestType',
 
-  fields: {
-    id: { type: GraphQLID },
-    code: { type: new GraphQLNonNull(GraphQLString) },
-    title: { type: new GraphQLNonNull(GraphQLString) },
-    description: { type: GraphQLString },
-    status: { type: new GraphQLNonNull(ContestStatusType) },
-    createdAt: { type: new GraphQLNonNull(GraphQLString) }
-
+  fields: () => {
+    const NameType = require('./name');
+    const ContestStatusType = require('./contest-status');
+    return {
+      id: {type: GraphQLID},
+      code: {type: new GraphQLNonNull(GraphQLString)},
+      title: {type: new GraphQLNonNull(GraphQLString)},
+      description: {type: GraphQLString},
+      status: {type: new GraphQLNonNull(ContestStatusType)},
+      createdAt: {type: new GraphQLNonNull(GraphQLString)},
+      names: {
+        type: new GraphQLList(NameType),
+        resolve(obj, args, {loaders}) {
+          return loaders.namesForContestIds.load(obj.id);
+        }
+      }
+    }
   }
-})
+});
