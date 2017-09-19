08676aa9afb90670c194f7790c45e21d54cdefda
Ivan Kipyegon <ivan@bardo.com>
2017-09-19 18:18:48 +0300

    chore(): Fix linting issues

--- schema/types/contest-status.js
+++ schema/types/contest-status.js
@@ -1,15 +1,13 @@
 const {
   GraphQLEnumType
-}= require('graphql');
+} = require('graphql');
 
 module.exports = new GraphQLEnumType({
-
   name: 'ContestStatusType',
 
   values: {
-    DRAFT: { value: 'draft'},
-    PUBLISHED: { value: 'published'},
+    DRAFT: { value: 'draft' },
+    PUBLISHED: { value: 'published' },
     ARCHIVED: { value: 'archived' }
   }
-
 });
